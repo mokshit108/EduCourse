54059e97d04d36ccff44e45cd2ca357d66c0a8bb
Mokshit Shah <mokshit108@gmail.com>
2025-08-10 16:51:09 +0530

    Next js config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   env: {
     GRAPHQL_API_URL: process.env.GRAPHQL_API_URL || 'http://localhost:4000/graphql',
   },
